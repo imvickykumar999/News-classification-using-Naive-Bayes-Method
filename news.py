8d6329a20a9320d758eb8d9dbc8db875d7945324
imvickykumar999 <imvickykumar999@gmail.com>
2023-12-05 20:52:28 +0530

    model saved and loaded

--- news.py
+++ news.py
@@ -1,30 +1,32 @@
 
-import numpy as np
-import pandas as pd
 from sklearn.datasets import fetch_20newsgroups
+import pickle
 
 data=fetch_20newsgroups()
 categories=data.target_names
 
 train=fetch_20newsgroups(subset='train',categories=categories)
-test=fetch_20newsgroups(subset='test',categories=categories)
+# test=fetch_20newsgroups(subset='test',categories=categories)
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import make_pipeline
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.pipeline import make_pipeline
 
-model=make_pipeline(TfidfVectorizer(),MultinomialNB())
-model.fit(train.data,train.target)
-labels=model.predict(test.data)
+# model=make_pipeline(TfidfVectorizer(),MultinomialNB())
+# model.fit(train.data,train.target)
 
-from sklearn.metrics import confusion_matrix
-mat=confusion_matrix(test.target,labels)
+# with open('model.pkl','wb') as f:
+#     pickle.dump(model,f)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 def predict_category(s,train=train,model=model):
     pred=model.predict([s])
     print(pred)
     return train.target_names[pred[0]]
 
-predict_category('''
-Determined to continue Gaza operation until aim met: Israeli PM
-''') 
+while True:
+    askme = input('\nEnter news headline : ')
+    predicted_news = predict_category(askme) 
+    print(predicted_news)
